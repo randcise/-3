4950c5b2532a3bd9f9608f926fb31e294bc0bef8
рандкаис <o.yakymchuk@istu.edu.ua>
2025-10-21 13:31:06 +0300

    Add files via upload

---
+++ Task5_ThreeDOperator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task5_ThreeDOperator
+{
+    class ThreeD
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public ThreeD(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // Перевизначення оператора +
+        public static ThreeD operator +(ThreeD a, ThreeD b) => new ThreeD(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+        public void Print() => Console.WriteLine($"({X}, {Y}, {Z})");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Перевизначення оператора + для ThreeD ===");
+            Console.WriteLine("Час виконання: 12:14 PM CEST, 21.10.2025");
+            try
+            {
+                var point1 = new ThreeD(1, 2, 3);
+                var point2 = new ThreeD(4, 5, 6);
+                var sum = point1 + point2;
+                sum.Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
+        }
+    }
+}
